183cc1289935a03893d9e34d0c69ca1ce000cdaa
Swift <swift@HEMANTs-MacBook-Pro.local>
2019-04-24 01:31:51 -0400

    Added view controllers and registered user

--- FinalProjectRender/FinalProjectRender/Controllers/RegisterViewController.swift
+++ FinalProjectRender/FinalProjectRender/Controllers/RegisterViewController.swift
@@ -7,9 +7,93 @@
 //
 
 import UIKit
+import CoreLocation
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTxt: UITextField!
+    @IBOutlet weak var lastNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var verPassTxt: UITextField!
+    @IBOutlet weak var phoneTxt: UITextField!
+    @IBOutlet weak var streetTxt: UITextField!
+    @IBOutlet weak var aptTxt: UITextField!
+    @IBOutlet weak var cityTxt: UITextField!
+    @IBOutlet weak var stateTxt: UITextField!
+    @IBOutlet weak var countryTxt: UITextField!
+    @IBOutlet weak var zipTxt: UITextField!
+    @IBAction func UploadProfileImg(_ sender: UIButton) {
+    }
+    @IBOutlet weak var rolePicker: UIPickerView!
+    func alert(_ message: String){
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert);
+        let OkAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(OkAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    @IBAction func registerBtn(_ sender: UIButton) {
+        if ((firstNameTxt?.text?.isEmpty)! || (lastNameTxt?.text?.isEmpty)! || (emailTxt?.text?.isEmpty)! || (usernameTxt?.text?.isEmpty)! || (passTxt?.text?.isEmpty)! || (verPassTxt?.text?.isEmpty)! || (phoneTxt?.text?.isEmpty)! || (streetTxt?.text?.isEmpty)! || (aptTxt?.text?.isEmpty)! || (cityTxt?.text?.isEmpty)!
+            || (stateTxt?.text?.isEmpty)! || (countryTxt?.text?.isEmpty)! || (zipTxt?.text?.isEmpty)!) {
+            alert("Values of the field can not be empty!")
+            return
+        }
+        
+        if (!(phoneTxt.text?.isInt)!) {
+            alert("Invalid phone")
+            return
+        }
+        
+        if (!(aptTxt.text?.isInt)!) {
+            alert("Invalid apartment number")
+            return
+        }
+        
+        if (!(zipTxt.text?.isInt)!) {
+            alert("Invalid zip")
+            return
+        }else if let number = Int(zipTxt.text!)
+        {
+            if((number < 0) || (number>Int.max))
+            {
+                alert("Invalid zip")
+                return
+            }
+        
+        }
+        
+        if (!(emailTxt.text?.isEmail)!) {
+            alert("Invalid email address")
+            return
+        }
+        
+        if (passTxt.text != verPassTxt.text)
+        {
+            alert("Password doesn't match")
+            return
+        }
+        
+        if (!(passTxt.text?.isPassword)!)
+        {
+            alert("Invalid password")
+            return
+        }
+        
+        let coor:String = "" + streetTxt.text! + aptTxt.text! + ", " + cityTxt.text! + ", " + stateTxt.text! + " " + zipTxt.text!
+        print(coor)
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(coor) { (placemarks, error) in
+            guard let placemarks = placemarks,let location = placemarks.first?.location
+                else {
+                    let alert = UIAlertController(title: "ALERT", message: "PLEASE ENTER A VALID ADDRESS!!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    return
+            }
+        
+    }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ FinalProjectRender/FinalProjectRender/Foundation/Util.swift
@@ -0,0 +1,56 @@
+//
+//  Util.swift
+//  FinalProjectRender
+//
+//  Created by Swift on 4/24/19.
+//  Copyright © 2019 Swift. All rights reserved.
+//
+
+import Foundation
+
+class Util {
+    
+    static func isValidEmail(email:String) -> Bool {
+        let regex = try? NSRegularExpression(pattern: "^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$", options: .caseInsensitive)
+        return regex?.firstMatch(in: email, options: [], range: NSMakeRange(0, email.count)) != nil
+    }
+    
+    static func isValidYear(year:String) -> Bool {
+        let regex = try? NSRegularExpression(pattern: "^([0-9]{4})$", options: .caseInsensitive)
+        return regex?.firstMatch(in: year, options: [], range: NSMakeRange(0, year.count)) != nil
+    }
+    
+    static func isPasswordValid(_ password : String) -> Bool{
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[@!])[A-Za-z\\d@!]{4,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    
+    
+}
+
+extension String {
+    var isInt: Bool {
+        return (Int(self) != nil && Int(self)! > 0)
+    }
+}
+
+extension String {
+    var isEmail: Bool {
+        return Util.isValidEmail(email: self)
+    }
+}
+
+extension String {
+    var isValidYear: Bool {
+        return Util.isValidYear(year: self)
+    }
+}
+
+extension String {
+    var isPassword: Bool {
+        return Util.isPasswordValid(self)
+    }
+}
+
+
